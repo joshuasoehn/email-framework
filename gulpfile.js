9bba108853092c6cc075a0fab9f39e251ee1fa43
Joshua <joshua.soehn@gmail.com>
2016-10-26 17:47:55 +0200

    added debugging and mailgun sending

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var inlineCss = require('gulp-inline-css');
 var fileinclude = require('gulp-file-include');
+var sendmail = require('gulp-mailgun');
+var gutil = require('gulp-util');
+
+var config = require('./config.json');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -31,6 +35,17 @@ gulp.task('build', function() {
 gulp.task('watch', function() {
   gulp.watch('./src/templates/*.html', ['build', reload]);
   gulp.watch('./src/templates/css/*.css', ['build', reload]);
+});
+
+// Add ability to send test emails
+gulp.task('send', function () {
+  gulp.src( './build/basic-template.html')
+  .pipe(sendmail({
+    key: config.auth.mailgun.apikey,
+    sender: config.testing.from,
+    recipient: config.testing.to,
+    subject: 'This is a test email'
+  }));
 });
 
 gulp.task('default', ['browser-sync', 'build', 'watch']);

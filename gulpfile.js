99c8815675638d0fdf194baa23fbacaf978dbd62
Joshua <joshua.soehn@gmail.com>
2016-10-26 15:32:13 +0200

    added build process, inline css

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var inlineCss = require('gulp-inline-css');
 
 // Static server
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: "./src",
-      index: "./templates/basic-template.html"
+      baseDir: "./src/templates/",
+      index: "basic-template.html"
     }
   });
 });
 
 // Build Templates
 gulp.task('build', function() {
-  return gulp.src('sc/templates/*.html')
+  return gulp.src('./src/templates/*.html')
+    .pipe(inlineCss())
+    .pipe(gulp.dest('./build/'));
 });
 
 // Watch Files For Changes And Reload
 gulp.task('watch', function() {
-  gulp.watch('src/templates/*.html', ['build', reload]);
-  gulp.watch('src/css/*.css', ['build', reload]);
+  gulp.watch('./src/templates/*.html', ['build', reload]);
+  gulp.watch('./src/templates/css/*.css', ['build', reload]);
 });
 
 gulp.task('default', ['browser-sync', 'build', 'watch']);
